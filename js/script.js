5079a30fd6915acbf1e5eb2bec20bce82af1fd7b
Giana <113945161+giana2022@users.noreply.github.com>
2023-10-25 16:41:17 -0300

    solucione footer alto en agradecimiento

--- js/script.js
+++ js/script.js
@@ -1,21 +1,3 @@
-/* function esVisible(elemento){
-    if (elemento.style.visibility == 'visible'){
-        return true
-    }
-    else {
-        return false;
-    }
-}
-function clickBoton(barra){
-    let esvisibleBarra = esVisible(barra);
-    if(esvisibleBarra===true){
-        barra.style.visibility = 'hidden'
-        console.log('visible')
-    }
-    else {
-        barra.style.visibility = 'visible';
-    }
-} */
 let boton = document.querySelector('.boton-menu');
 let nav = document.querySelector('.nav-inicio');
 
