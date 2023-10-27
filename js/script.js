a8d299e4f275232a0a34b4c855fa343b7ed28e6b
Giana <113945161+giana2022@users.noreply.github.com>
2023-10-27 20:53:25 -0300

    Valida regexp (#13)
    * Nueva rama para script
    
    * Agregue validacion de nombre por expregular

--- js/script.js
+++ js/script.js
@@ -12,3 +12,52 @@ boton.addEventListener('click',()=>{
     }
 });
 
+/* function validar(nombr, mail) {
+    let nombre = document.forms["Form"]["nombre"].value;
+    let email = document.forms["Form"]["email"].value;
+    let mensaje = document.forms["Form"]["mensaje"].value;
+    if (nombre == "") {
+      alert("Escribe tu nombre por favor...");
+      return false;
+    }
+    if (email == "") {
+      alert("Escribe tu email por favor...");
+      return false;
+    }
+    if (mensaje == "") {
+      alert("Escribe un mensaje por favor...");
+      return false;
+    }
+    if(nombre!==""){
+      let boolean = validarTexto(nombr, mail);
+      if (boolean == false) {
+        return false
+      }
+    }
+  }
+
+function validarTexto(nombre, email) {
+    let valorNombre = nombre.value;
+   // let valorEmail = email.value;
+    let reg = new RegExp(/^([a-zA-Z]+)(\s[a-zA-Z]+)*$/);
+   // let regemail = new RegExp('/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/');
+    if (reg.test(valorNombre)){
+        console.log(valorNombre, 'si coincide')
+        return true
+    }
+    else {
+        console.log(valorNombre, 'no coincide')
+        alert('Valor incorrecto solo se admiten letras')
+        console.log(false)
+        return false
+    }
+   
+}
+
+let nombre = document.getElementById('nombre'),
+email = document.getElementById('email'),
+enviar = document.getElementById('enviar');
+let form = document.getElementById('formulario');
+enviar.addEventListener("click", function(){
+  validar(nombre, email)
+  /* validarTexto() */
