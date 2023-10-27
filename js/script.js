ef0c47da79f38415716750db184511b9a6883a58
Giana <113945161+giana2022@users.noreply.github.com>
2023-10-27 14:04:59 -0300

    Nueva rama para script

--- js/script.js
+++ js/script.js
@@ -12,3 +12,55 @@ boton.addEventListener('click',()=>{
     }
 });
 
+function validar() {
+    let nombre = document.forms["Form"]["nombre"].value;
+    let email = document.forms["Form"]["email"].value;
+    let mensaje = document.forms["Form"]["mensaje"].value;
+    if (nombre == "") {
+      alert("Escribe tu nombre por favor...");
+      return false;
+    }
+    if (email == "") {
+      alert("Escribe tu email por favor...");
+      return false;
+    }
+    if (mensaje == "") {
+      alert("Escribe un mensaje por favor...");
+      return false;
+    }
+  }
+
+function validarTexto(nombre, email) {
+    let valorNombre = nombre.value;
+   // let valorEmail = email.value;
+    let reg = new RegExp(/^([a-zA-Z]+)(\s[a-zA-Z]+)*$/);
+   // let regemail = new RegExp('/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/');
+    if (reg.test(valorNombre)){
+        console.log(valorNombre, 'si coincide')
+        /*
+        if (regemail.test(valorEmail)){
+            console.log(valorEmail, 'si coincide')
+            return true
+        }
+        else {
+            console.log(valorEmail, 'no coincide')
+            alert('Valor incorrecto para mail')
+            return false
+        } */
+    }
+    else {
+        console.log(valorNombre, 'no coincide')
+        alert('Valor incorrecto solo se admiten letras')
+        return false
+    }
+   
+}
+
+let nombre = document.getElementById('nombre'),
+email = document.getElementById('email'),
+enviar = document.getElementById('enviar');
+let form = document.getElementById('formulario');
+enviar.addEventListener("click", function(){
+  validar()
+  validarTexto(nombre, email)
+})
